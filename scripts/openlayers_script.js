2745983beebd83c2891237c00f71e66e74ed28c8
Justin R <justin.relitz@gmail.com>
2015-02-18 20:23:52 -0700

    This is to change some of the map settings

--- scripts/openlayers_script.js
+++ scripts/openlayers_script.js
@@ -36,7 +36,7 @@ var map = new ol.Map({
   ]),
   layers: [raster, mtc, temples],
   renderer: exampleNS.getRendererFromQueryString(),
-  target: 'map',
+  target: document.getElementById('map'),
   view: new ol.View({
     center: [876970.8463461736, 5859807.853963373],
     projection: projection,
